cfbbe9ca8111352a44bf1969ca24b061bde522ea
Stepan <115095505+HappySaber@users.noreply.github.com>
2023-03-26 22:25:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,3 @@
-// Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len-1; i++) {
@@ -12,37 +11,52 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int* arr, int len, int value) {
     int count = 0;
+    int j, i;
     for (int i = 0; i < len - 1; i++) {
       for (int j = len - 1; j > i; j--) {
         if (arr[i] + arr[j] == value)
             count++;
-    }
+      }
+      if (j - i == 1 && ((arr[j] + arr[i]) > value)) return count;
     }
     return count;
 }
 
-
+int binarniypoisk(int* arr, int lens, int value) {
+    int r = 0;
+    int s = 0;
+    int e = lens - 1;
+    while (s <= e) {
+    int mid = (s + e) / 2;
+    if ((arr[mid]) == value) {
+        r += 1;
+        int mid2 = mid;
+        if (mid != lens - 1) {
+          while (((arr[mid] + 1)) == value) {
+            r += 1;
+            mid += 1;
+          }
+        }
+        if (mid2) {
+          while ((arr[mid2] - 1) == value) {
+            r += 1;
+            mid2 -= 1;
+            if (mid2 == 0) break;
+          }
+        }
+        return r;
+    } else if (arr[mid] > value) {
+        e = mid - 1;
+    } else {
+        s = mid + 1;
+    }
+    }
+    return r;
+}
 
 int countPairs3(int* arr, int len, int value) {
     int count = 0;
-    for (int i = len; i < len - 1; i++) {
-    int l = i, r = len;
-    while (r - 1 > l) {
-        int m = (l + r) / 2;
-        if (arr[i] + arr[m] == value) {
-          count++;
-          int j = m+1;
-          while (arr[i] + arr[j] == value && j < r) {
-            count++;
-            j--;
-          }
-          break;
-        }
-        if (arr[i] + arr[m] > value)
-          r = m;
-        else
-          l = m;
-    }
-    }
+    for (int i = 0; i < len - 1 && arr[i] <= value; i++)
+      count += binarniypoisk(arr + i + 1, len - i - 1, value - arr[i]);
     return count;
 }
